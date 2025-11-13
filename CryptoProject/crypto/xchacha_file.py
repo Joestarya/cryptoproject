e87f0e90ccf8903454f1428e8ed7ce9da87e1d13
chys <milathina@gmail.com>
2025-11-13 14:34:35 +0700

    finish

--- CryptoProject/crypto/xchacha_file.py
+++ CryptoProject/crypto/xchacha_file.py
@@ -1,7 +1,7 @@
 # crypto/xchacha_file.py
 import base64
 import os
-from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305 as XChaCha20Poly1305
+from cryptography.hazmat.primitives.ciphers.aead import XChaCha20Poly1305
 
 def generate_key_b64() -> str:
     key = os.urandom(32)
@@ -21,7 +21,7 @@ def encrypt_file_bytes(file_bytes: bytes, key_b64: str):
         if len(key) != 32:
             raise ValueError("Kunci harus berukuran 32 byte saat didekripsi dari base64!")
         aead = XChaCha20Poly1305(key)
-        nonce = os.urandom(12)
+        nonce = os.urandom(24)
         ct = aead.encrypt(nonce, file_bytes, associated_data=None)
         return base64.b64encode(ct).decode(), base64.b64encode(nonce).decode()
     except Exception as e:
@@ -34,7 +34,7 @@ def decrypt_file_bytes(ciphertext_b64: str, nonce_b64: str, key_b64: str) -> byt
         ct = _b64decode_str(ciphertext_b64)
         if len(key) != 32:
             raise ValueError("Kunci harus berukuran 32 byte saat didekripsi dari base64!")
-        if len(nonce) != 12:
+        if len(nonce) != 24:
             raise ValueError("Nonce harus berukuran 24 byte saat didekripsi dari base64!")
         if len(ct) < 16:
             raise ValueError("Ciphertext terlalu pendek / tidak valid.")
